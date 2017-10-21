a7a70fd225df04d97124d0240390e6d31ef33e65
Wendy <zhaod.wendy@gmail.com>
2017-10-21 14:39:03 -0700

    add framework for single page app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express"),
 //Express set-up
 
 app.set("view engine", "ejs")
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 //Routes
@@ -13,6 +13,8 @@ app.get("/", function(req,res){
 	res.render("index");
 })
 
+
 app.listen(3000, function(){
 	console.log("Server is running!");
 })
+
